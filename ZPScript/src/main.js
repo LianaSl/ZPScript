36c973f82145a88eb9b30e35bf50f07b635d8c2a
LianaSl <sljonkina@gmail.com>
2026-05-10 09:41:09 +0200

    update

--- ZPScript/src/main.js
+++ ZPScript/src/main.js
@@ -4,5 +4,6 @@ const API_URL = "https://pixabay.com/api"
 const KEY_API = "55790798-f52520810e523ed63afe7ae87"
 const lang = "cs"
 const inputValue = "kytice";
+fetch(`${API_URL}?key=${KEY_API}&q=${kytice}&lang=${lang}`);
 
-const res = await fetch(`${API_URL}?key=${KEY_API}&q=${inputValue}&lang=${lang}`);
+const res = await fetch(`${API_URL}?key=${KEY_API}&q=${kytice}&lang=${lang}`);
